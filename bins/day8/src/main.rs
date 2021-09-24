a2c3aa1c4b92129eae8f4a5e898264afa3b544fb
Fang He <hf.enigma@gmail.com>
2021-09-24 15:22:52 +0800

    day 8

---
+++ bins/day8/src/main.rs
@@ -0,0 +1,98 @@
+use anyhow::Result;
+use regex::Regex;
+use std::{
+    env, fs,
+    io::{self, BufRead},
+};
+
+#[derive(Clone)]
+enum Instruction {
+    Jmp(i32),
+    Acc(i32),
+    Nop(i32),
+}
+
+fn parse(line: &str, re: &Regex) -> Instruction {
+    if let Some(cap) = re.captures(line) {
+        let op = &cap[1];
+        let arg = cap[2].parse::<i32>().expect("invalid argument");
+        match op {
+            "jmp" => Instruction::Jmp(arg),
+            "acc" => Instruction::Acc(arg),
+            "nop" => Instruction::Nop(arg),
+            _ => panic!("invalid op code: {}", op),
+        }
+    } else {
+        panic!("invalid line: {}", line);
+    }
+}
+
+fn run(instructions: &[Instruction], mut pos: usize) -> (i32, usize) {
+    let mut acc = 0;
+    let n = instructions.len();
+    let mut visited = vec![false; n];
+    while pos < n && !visited[pos] {
+        let offset: i32 = match instructions[pos] {
+            Instruction::Acc(value) => {
+                acc += value;
+                1
+            }
+            Instruction::Jmp(value) => value,
+            Instruction::Nop(_) => 1,
+        };
+        visited[pos] = true;
+        if pos == n - 1 {
+            break;
+        }
+        pos = (pos as i32 + offset) as usize;
+    }
+    (acc, pos)
+}
+
+fn find_bug(instructions: &mut [Instruction]) -> i32 {
+    let len = instructions.len() - 1;
+    for i in 0..=len {
+        match instructions[i] {
+            Instruction::Jmp(v) => instructions[i] = Instruction::Nop(v),
+            Instruction::Nop(v) => instructions[i] = Instruction::Jmp(v),
+            Instruction::Acc(_) => continue,
+        }
+
+        let (acc, pos) = run(instructions, 0);
+        if pos == len {
+            return acc;
+        }
+
+        match instructions[i] {
+            Instruction::Jmp(v) => instructions[i] = Instruction::Nop(v),
+            Instruction::Nop(v) => instructions[i] = Instruction::Jmp(v),
+            Instruction::Acc(_) => continue,
+        }
+    }
+    panic!("no solutions found")
+}
+
+fn main() -> Result<()> {
+    let file_path = env::args()
+        .nth(1)
+        .expect("not enough arguments, missing data file path");
+    let data_file = fs::File::open(file_path)?;
+
+    let r1 = Regex::new(r"^(\w{3}) ([+|-]\d+)$").unwrap();
+
+    let mut instructions = io::BufReader::new(data_file)
+        .lines()
+        .flatten()
+        .map(|line| parse(&line, &r1))
+        .collect::<Vec<_>>();
+
+    // part 1
+    let acc = run(&instructions, 0).0;
+    println!("accumulator: {}", acc);
+
+    // part 2
+    let acc = find_bug(&mut instructions);
+    println!("accumulator part2: {}", acc);
+
+    Ok(())
+}
