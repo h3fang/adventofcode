e2f7bf65bfef52ca4f9f13193c7386686b8b88e6
Fang He <hf.enigma@gmail.com>
2021-09-25 14:05:10 +0800

    measure execution time

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use adventofcode::*;
 use anyhow::Result;
+use std::time::Instant;
 
 fn main() -> Result<()> {
-    day1::main("data/day1")?;
-    day2::main("data/day2")?;
-    day3::main("data/day3")?;
-    day4::main("data/day4")?;
-    day5::main("data/day5")?;
-    day6::main("data/day6")?;
-    day7::main("data/day7")?;
-    day8::main("data/day8")?;
-    day9::main("data/day9")?;
-    day10::main("data/day10")?;
+    let fns: Vec<(fn(&str) -> Result<()>, &str)> = vec![
+        (day1::main, "data/day1"),
+        (day2::main, "data/day2"),
+        (day3::main, "data/day3"),
+        (day4::main, "data/day4"),
+        (day5::main, "data/day5"),
+        (day6::main, "data/day6"),
+        (day7::main, "data/day7"),
+        (day8::main, "data/day8"),
+        (day9::main, "data/day9"),
+        (day10::main, "data/day10"),
+    ];
+
+    fns.iter().for_each(|(f, path)| {
+        let start = Instant::now();
+        f(path).unwrap();
+        println!("time: {:?}\n", Instant::now() - start);
+    });
 
     Ok(())
 }
