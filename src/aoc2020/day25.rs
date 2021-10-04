8703a347cfea6c238b008569630dae6b30f8ea00
Fang He <hf.enigma@gmail.com>
2021-10-04 09:45:02 +0800

    day 25

---
+++ src/aoc2020/day25.rs
@@ -0,0 +1,60 @@
+const MODULO: usize = 20201227;
+const SUBJECT: usize = 7;
+
+fn parse(content: &str) -> (usize, usize) {
+    let nums = content
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<_>>();
+    (nums[0], nums[1])
+}
+
+#[inline]
+fn round(value: usize, subject: usize) -> usize {
+    (value * subject) % MODULO
+}
+
+fn loop_size(pubkey: usize) -> usize {
+    let mut s = 1;
+    let mut v = 1;
+    loop {
+        v = round(v, SUBJECT);
+        if v == pubkey {
+            return s;
+        }
+        s += 1;
+    }
+}
+
+fn part1(card_pub: usize, door_pub: usize) -> usize {
+    let card_loop = loop_size(card_pub);
+    // let door_loop = loop_size(door_pub);
+    let mut v = 1;
+    for _ in 0..card_loop {
+        v = round(v, door_pub);
+    }
+    v
+}
+
+pub fn main() {
+    let content = std::fs::read_to_string("data/2020/day25").unwrap();
+    let (card_pub, door_pub) = parse(&content);
+
+    // part 1
+    println!("day 25 part1: {}", part1(card_pub, door_pub));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn input1() {
+        let content = "5764801\n17807724";
+        let (card_pub, door_pub) = parse(&content);
+
+        // part 1
+        assert_eq!(14897079, part1(card_pub, door_pub));
+    }
+}
--- src/aoc2020/mod.rs
+++ src/aoc2020/mod.rs
@@ -15,6 +15,7 @@ mod day21;
 mod day22;
 mod day23;
 mod day24;
+mod day25;
 mod day3;
 mod day4;
 mod day5;
@@ -48,4 +49,5 @@ pub const DAYS: &[fn()] = &[
     day22::main,
     day23::main,
     day24::main,
+    day25::main,
 ];
