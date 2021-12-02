4c39fc32e27b182261e791e7f4ee5e0e1a4df840
Fang He <hf.enigma@gmail.com>
2021-12-02 23:01:28 +0800

    2021 day 2

---
+++ src/aoc2021/day2.rs
@@ -0,0 +1,72 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Command {
+    Up(i64),
+    Down(i64),
+    Forward(i64),
+}
+
+#[derive(Debug)]
+struct InvalidCommand(String);
+
+impl FromStr for Command {
+    type Err = InvalidCommand;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.trim().split(' ');
+        let dir = parts.next().ok_or_else(|| InvalidCommand(s.to_string()))?;
+        let n = parts
+            .next()
+            .ok_or_else(|| InvalidCommand(s.to_string()))?
+            .parse::<i64>()
+            .map_err(|_| InvalidCommand(s.to_string()))?;
+        match dir {
+            "up" => Ok(Command::Up(n)),
+            "down" => Ok(Command::Down(n)),
+            "forward" => Ok(Command::Forward(n)),
+            _ => Err(InvalidCommand(s.to_string())),
+        }
+    }
+}
+
+fn part1(cmds: &[Command]) -> i64 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    for cmd in cmds {
+        match cmd {
+            Command::Up(n) => depth -= n,
+            Command::Down(n) => depth += n,
+            Command::Forward(n) => horizontal += n,
+        }
+    }
+    horizontal * depth
+}
+
+fn part2(cmds: &[Command]) -> i64 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for cmd in cmds {
+        match cmd {
+            Command::Up(n) => aim -= *n,
+            Command::Down(n) => aim += *n,
+            Command::Forward(n) => {
+                horizontal += n;
+                depth += aim * n;
+            }
+        }
+    }
+    horizontal * depth
+}
+
+pub fn main() {
+    let cmds: Vec<Command> = std::fs::read_to_string("data/2021/day2")
+        .unwrap()
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!("day2 part1: {}", part1(&cmds));
+    println!("day2 part2: {}", part2(&cmds));
+}
--- src/aoc2021/main.rs
+++ src/aoc2021/main.rs
@@ -1,6 +1,7 @@
 mod day1;
+mod day2;
 
-pub const DAYS: &[fn()] = &[day1::main];
+pub const DAYS: &[fn()] = &[day1::main, day2::main];
 
 use std::time::Instant;
 
