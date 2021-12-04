343cafa09d538761f2774dbc05bf0926d1e8cf5f
Fang He <hf.enigma@gmail.com>
2021-12-04 12:27:47 +0800

    run specified day if provided

--- src/aoc2021/main.rs
+++ src/aoc2021/main.rs
@@ -7,9 +7,20 @@ pub const DAYS: &[fn()] = &[day1::main, day2::main, day3::main];
 use std::time::Instant;
 
 fn main() {
-    DAYS.iter().for_each(|f| {
-        let start = Instant::now();
-        f();
-        println!("time: {:?}\n", Instant::now() - start);
-    });
+    if let Some(day) = std::env::args().nth(1) {
+        let day = day.parse::<usize>().expect("invalid day");
+        if day >= 1 && day <= DAYS.len() {
+            let start = Instant::now();
+            DAYS[day - 1]();
+            println!("time: {:?}\n", Instant::now() - start);
+        } else {
+            panic!("invalid day");
+        }
+    } else {
+        DAYS.iter().for_each(|f| {
+            let start = Instant::now();
+            f();
+            println!("time: {:?}\n", Instant::now() - start);
+        });
+    }
 }
