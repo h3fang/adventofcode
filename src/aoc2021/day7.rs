45f5fc1d8b086d480d8cebca2d3895c820bc0a93
Fang He <hf.enigma@gmail.com>
2021-12-07 15:24:04 +0800

    2021 day 7

---
+++ src/aoc2021/day7.rs
@@ -0,0 +1,35 @@
+fn part1(pos: &[i64]) -> i64 {
+    let n = pos.len();
+    let m = pos[n / 2];
+    pos.iter().map(|p| (p - m).abs()).sum()
+}
+
+fn part2(pos: &[i64]) -> i64 {
+    let n = pos.len();
+    let sum = pos.iter().sum::<i64>();
+    let avg = sum / n as i64;
+    (avg..=avg + 1)
+        .map(|avg| {
+            pos.iter()
+                .map(|p| {
+                    let d = (p - avg).abs();
+                    d * (d + 1) / 2
+                })
+                .sum::<i64>()
+        })
+        .min()
+        .unwrap()
+}
+
+pub fn main() {
+    let data = std::fs::read_to_string("data/2021/day7").unwrap();
+    let mut pos = data
+        .trim()
+        .split(',')
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    pos.sort_unstable();
+    println!("day7 part1: {}", part1(&pos));
+    println!("day7 part2: {}", part2(&pos));
+}
--- src/aoc2021/main.rs
+++ src/aoc2021/main.rs
@@ -4,6 +4,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 pub const DAYS: &[fn()] = &[
     day1::main,
@@ -12,6 +13,7 @@ pub const DAYS: &[fn()] = &[
     day4::main,
     day5::main,
     day6::main,
+    day7::main,
 ];
 
 use std::time::Instant;
