3e2023fae2bc2de2098b38457582d1cfe27f309e
Fang He <hf.enigma@gmail.com>
2021-12-27 16:37:54 +0800

    faster indexing

--- src/aoc2021/day25.rs
+++ src/aoc2021/day25.rs
@@ -12,20 +12,18 @@ struct Grid {
 }
 
 impl Grid {
-    fn step(&mut self) -> usize {
-        let mut changed = 0;
+    fn step(&mut self) -> bool {
+        let mut changed = false;
         let mut next = self.grid.clone();
-
-        let index = |x: usize, y: usize| -> usize { y * self.width + x };
 
         // east
         for y in 0..self.height {
             for x in 0..self.width {
-                let i1 = index(x, y);
-                let i2 = index((x + 1) % self.width, y);
+                let i1 = y * self.width + x;
+                let i2 = if x + 1 == self.width { i1 - x } else { i1 + 1 };
                 if Cell::East == self.grid[i1] && self.grid[i2] == Cell::Empty {
                     next.swap(i1, i2);
-                    changed += 1;
+                    changed = true;
                 }
             }
         }
@@ -35,11 +33,15 @@ impl Grid {
         // south
         for y in 0..self.height {
             for x in 0..self.width {
-                let i1 = index(x, y);
-                let i2 = index(x, (y + 1) % self.height);
+                let i1 = y * self.width + x;
+                let i2 = if y + 1 == self.height {
+                    x
+                } else {
+                    i1 + self.width
+                };
                 if Cell::South == next[i1] && next[i2] == Cell::Empty {
                     self.grid.swap(i1, i2);
-                    changed += 1;
+                    changed = true;
                 }
             }
         }
@@ -72,13 +74,10 @@ fn parse(data: &str) -> Grid {
 
 fn part1(grid: &mut Grid) -> usize {
     let mut i = 1;
-    loop {
-        let changed = grid.step();
-        if changed == 0 {
-            return i;
-        }
+    while grid.step() {
         i += 1;
     }
+    i
 }
 
 pub fn main() {
