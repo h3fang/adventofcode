87babb31ee8bb6e5127ef73163b2ad404dc7735c
Fang He <hf.enigma@gmail.com>
2021-12-30 18:17:28 +0800

    compact representation

--- src/aoc2020/day23.rs
+++ src/aoc2020/day23.rs
@@ -1,30 +1,30 @@
-fn parse(content: &str) -> (Vec<usize>, Vec<usize>) {
+fn parse(content: &str) -> (Vec<u32>, Vec<u32>) {
     let nums = content
         .chars()
         .filter_map(|c| c.to_digit(10))
-        .map(|n| n as usize)
+        .map(|n| n as u32)
         .collect::<Vec<_>>();
     let mut r1 = vec![0; nums.len() + 1];
     let mut prev = 0;
     for &n in &nums {
         r1[prev] = n;
-        prev = n;
+        prev = n as usize;
     }
     r1[prev] = r1[0];
 
     let mut r2 = r1.clone();
     let one_million = 1000000;
-    r2[prev] = nums.len() + 1;
+    r2[prev] = nums.len() as u32 + 1;
     r2.reserve(one_million + 1);
     for i in nums.len() + 1..one_million {
-        r2.push(i + 1);
+        r2.push(i as u32 + 1);
     }
     r2.push(r2[0]);
 
     (r1, r2)
 }
 
-// fn print_cups(c: &[usize]) {
+// fn print_cups(c: &[u32]) {
 //     let mut next = c[0];
 //     loop {
 //         print!("{}", next);
@@ -35,15 +35,15 @@ fn parse(content: &str) -> (Vec<usize>, Vec<usize>) {
 //     }
 // }
 
-fn make_moves(cups: &mut Vec<usize>, moves: usize) {
+fn make_moves(cups: &mut [u32], moves: u32) {
     let n = cups.len();
-    let mut current = cups[0];
+    let mut current = cups[0] as usize;
     for _ in 0..moves {
         // print_cups(c);
 
-        let c1 = cups[current];
-        let c2 = cups[c1];
-        let c3 = cups[c2];
+        let c1 = cups[current] as usize;
+        let c2 = cups[c1] as usize;
+        let c3 = cups[c2] as usize;
 
         // print!(" ({},{},{})", c1, c2 ,c3);
 
@@ -61,32 +61,32 @@ fn make_moves(cups: &mut Vec<usize>, moves: usize) {
         // println!(" {}", dest);
 
         let t = cups[dest];
-        cups[dest] = c1;
+        cups[dest] = c1 as u32;
         cups[current] = cups[c3];
         cups[c3] = t;
 
-        current = cups[current];
+        current = cups[current] as usize;
     }
 }
 
-fn part1(cups: &mut Vec<usize>) -> usize {
+fn part1(cups: &mut [u32]) -> usize {
     make_moves(cups, 100);
 
     // result
     let mut c = 1;
     let mut r = 0;
     while cups[c] != 1 {
-        c = cups[c];
+        c = cups[c] as usize;
         r = r * 10 + c;
     }
     r
 }
 
-fn part2(cups: &mut Vec<usize>) -> usize {
+fn part2(cups: &mut [u32]) -> usize {
     make_moves(cups, 10000000);
 
     // result
-    cups[1] * cups[cups[1]]
+    cups[1] as usize * cups[cups[1] as usize] as usize
 }
 
 pub fn main() {
