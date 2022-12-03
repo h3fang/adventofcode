55afde77984eb02cc03433b4adaf490d0a911560
Fang He <hf.enigma@gmail.com>
2022-12-03 13:45:28 +0800

    2022 day 3

---
+++ src/aoc2022/day3.rs
@@ -0,0 +1,63 @@
+fn parse(data: &str) -> Vec<&[u8]> {
+    data.trim().lines().map(|line| line.as_bytes()).collect()
+}
+
+fn priority(item: u8) -> u8 {
+    if (b'a'..=b'z').contains(&item) {
+        (item - b'a') + 1
+    } else {
+        (item - b'A') + 27
+    }
+}
+
+fn hash(items: &[u8]) -> u64 {
+    items.iter().fold(0, |h, &item| h | (1 << priority(item)))
+}
+
+fn part1(rucksacks: &[&[u8]]) -> u32 {
+    rucksacks
+        .iter()
+        .map(|r| {
+            let n = r.len() / 2;
+            let h = hash(&r[..n]) & hash(&r[n..]);
+            h.trailing_zeros()
+        })
+        .sum()
+}
+
+fn part2(rucksacks: &[&[u8]]) -> u32 {
+    rucksacks
+        .chunks_exact(3)
+        .map(|g| {
+            let h = hash(g[0]) & hash(g[1]) & hash(g[2]);
+            h.trailing_zeros()
+        })
+        .sum()
+}
+
+pub fn main() {
+    let data = std::fs::read_to_string("data/2022/day3").unwrap();
+    let rucksacks = parse(&data);
+    println!("part1: {}", part1(&rucksacks));
+    println!("part2: {}", part2(&rucksacks));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let data = "
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+            .to_string();
+        let rucksacks = parse(&data);
+        assert_eq!(157, part1(&rucksacks));
+        assert_eq!(70, part2(&rucksacks));
+    }
+}
