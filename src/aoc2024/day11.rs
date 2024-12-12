9acdea42e1dd4eba8218accc8176d82a09337ae7
Fang He <hf.enigma@gmail.com>
2024-12-12 09:08:52 +0800

    2024 day 11: minor optimization

--- src/aoc2024/day11.rs
+++ src/aoc2024/day11.rs
@@ -7,34 +7,41 @@ fn parse(input: &str) -> Vec<u64> {
         .collect()
 }
 
-fn blink(stones: &[u64], times: usize) -> usize {
+fn blink(stones: &[u64], times: &[usize]) -> Vec<usize> {
     let mut m: HashMap<u64, usize> = HashMap::with_capacity(stones.len());
     for &x in stones {
         *m.entry(x).or_insert(0) += 1;
     }
-    for _ in 0..times {
-        let mut next = HashMap::with_capacity(m.len() * 2);
-        for (x, c) in m {
-            if x == 0 {
-                *next.entry(1).or_default() += c;
-            } else if x.ilog10() % 2 == 1 {
-                let base = 10u64.pow((x.ilog10() + 1) / 2);
-                *next.entry(x / base).or_default() += c;
-                *next.entry(x % base).or_default() += c;
-            } else {
-                *next.entry(x * 2024).or_default() += c;
+    let mut blinks = 0;
+    let mut result = Vec::with_capacity(times.len());
+    for &t in times {
+        while blinks < t {
+            let mut next = HashMap::with_capacity(m.len() * 2);
+            for (x, c) in m {
+                if x == 0 {
+                    *next.entry(1).or_default() += c;
+                } else if x.ilog10() % 2 == 1 {
+                    let base = 10u64.pow((x.ilog10() + 1) / 2);
+                    *next.entry(x / base).or_default() += c;
+                    *next.entry(x % base).or_default() += c;
+                } else {
+                    *next.entry(x * 2024).or_default() += c;
+                }
             }
+            m = next;
+            blinks += 1;
         }
-        m = next;
+        result.push(m.values().sum());
     }
-    m.values().sum()
+    result
 }
 
 pub fn main() {
     let input: String = std::fs::read_to_string("data/2024/day11").unwrap();
     let stones = parse(&input);
-    println!("part1: {}", blink(&stones, 25));
-    println!("part2: {}", blink(&stones, 75));
+    let result = blink(&stones, &[25, 75]);
+    println!("part1: {}", result[0]);
+    println!("part2: {}", result[1]);
 }
 
 #[cfg(test)]
@@ -45,7 +52,6 @@ mod tests {
     fn case1() {
         let input = "125 17";
         let stones = parse(input);
-        assert_eq!(22, blink(&stones, 6));
-        assert_eq!(55312, blink(&stones, 25));
+        assert_eq!(vec![22, 55312], blink(&stones, &[6, 25]));
     }
 }
