579a4771348727bd81a7a92679685bd2cccb6aa3
Fang He <hf.enigma@gmail.com>
2024-12-20 16:59:50 +0800

    2024 day 20: optimization

--- src/aoc2024/day20.rs
+++ src/aoc2024/day20.rs
@@ -1,21 +1,16 @@
 use std::{cmp::Reverse, collections::BinaryHeap};
 
+use rayon::prelude::*;
+
 fn parse(input: &str) -> Vec<&[u8]> {
     input.trim().lines().map(|line| line.as_bytes()).collect()
 }
 
-fn find_start_end(map: &[&[u8]]) -> ((i32, i32), (i32, i32)) {
-    let mut start = (-1, -1);
-    let mut end = (-1, -1);
+fn find_start(map: &[&[u8]]) -> (i32, i32) {
     for (i, r) in map.iter().enumerate() {
         for (j, &c) in r.iter().enumerate() {
             if c == b'S' {
-                start = (i as i32, j as i32);
-            } else if c == b'E' {
-                end = (i as i32, j as i32);
-            }
-            if start != (-1, -1) && end != (-1, -1) {
-                return (start, end);
+                return (i as i32, j as i32);
             }
         }
     }
@@ -53,65 +48,73 @@ fn dijkstra(map: &[&[u8]], (i0, j0): (i32, i32)) -> Vec<i32> {
     dist
 }
 
-fn solve(map: &[&[u8]], save: i32) -> (usize, usize) {
+fn cheat(map: &[&[u8]], dist: &[i32], i: i32, j: i32, save: i32) -> (usize, usize) {
     let (m, n) = (map.len(), map[0].len());
-
-    let (start, end) = find_start_end(map);
-    let from_start = dijkstra(map, start);
-    let from_end = dijkstra(map, end);
-    let fastest = from_start[end.0 as usize * n + end.1 as usize];
     let (mut p1, mut p2) = (0, 0);
+    let d1 = dist[i as usize * n + j as usize];
 
-    let is_valid = |i: i32, j: i32| i >= 0 && j >= 0 && i < m as i32 && j < n as i32;
+    for (i1, j1) in [
+        (i - 2, j),
+        (i - 1, j - 1),
+        (i - 1, j + 1),
+        (i, j - 2),
+        (i, j + 2),
+        (i + 1, j - 1),
+        (i + 1, j + 1),
+        (i + 2, j),
+    ] {
+        if i1 < 0
+            || j1 < 0
+            || i1 >= m as i32
+            || j1 >= n as i32
+            || map[i1 as usize][j1 as usize] == b'#'
+        {
+            continue;
+        }
+        let d2 = dist[i1 as usize * n + j1 as usize];
+        if d2 - d1 - 2 >= save {
+            p1 += 1;
+        }
+    }
 
-    for (i, r) in map.iter().enumerate() {
-        for (j, &c) in r.iter().enumerate() {
-            if c == b'#' {
+    for i1 in i - 20..=i + 20 {
+        let di = (i1 - i).abs();
+        for j1 in j - (20 - di)..=j + 20 - di {
+            let d = di + (j1 - j).abs();
+            if i1 < 0
+                || j1 < 0
+                || i1 >= m as i32
+                || j1 >= n as i32
+                || map[i1 as usize][j1 as usize] == b'#'
+            {
                 continue;
             }
-            let d1 = from_start[i * n + j];
-            let (i, j) = (i as i32, j as i32);
-
-            for (i1, j1) in [
-                (i - 2, j),
-                (i - 1, j - 1),
-                (i - 1, j + 1),
-                (i, j - 2),
-                (i, j + 2),
-                (i + 1, j - 1),
-                (i + 1, j + 1),
-                (i + 2, j),
-            ] {
-                if is_valid(i1, j1) && map[i1 as usize][j1 as usize] != b'#' {
-                    let d2 = from_end[i1 as usize * n + j1 as usize];
-                    if d1 + d2 + 2 + save <= fastest {
-                        p1 += 1;
-                    }
-                }
-            }
-
-            for i1 in i - 20..=i + 20 {
-                let di = (i1 - i).abs();
-                for j1 in j - (20 - di)..=j + 20 - di {
-                    let d = di + (j1 - j).abs();
-                    if i1 < 0
-                        || j1 < 0
-                        || i1 >= m as i32
-                        || j1 >= n as i32
-                        || map[i1 as usize][j1 as usize] == b'#'
-                    {
-                        continue;
-                    }
-                    let d2 = from_end[i1 as usize * n + j1 as usize];
-                    if d1 + d2 + d + save <= fastest {
-                        p2 += 1;
-                    }
-                }
+            let d2 = dist[i1 as usize * n + j1 as usize];
+            if d2 - d1 - d >= save {
+                p2 += 1;
             }
         }
     }
 
     (p1, p2)
+}
+
+fn solve(map: &[&[u8]], save: i32) -> (usize, usize) {
+    let start = find_start(map);
+    let dist = dijkstra(map, start);
+    let (m, n) = (map.len(), map[0].len());
+
+    (0..m * n)
+        .into_par_iter()
+        .map(|k| {
+            let (i, j) = (k / n, k % n);
+            if map[i][j] == b'#' {
+                (0, 0)
+            } else {
+                cheat(map, &dist, i as i32, j as i32, save)
+            }
+        })
+        .reduce(|| (0, 0), |(p1, p2), (a, b)| (p1 + a, p2 + b))
 }
 
 pub fn main() {
