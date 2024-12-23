9b940940d9018fb2e089ead742109a6155ea5b21
Fang He <hf.enigma@gmail.com>
2024-12-23 23:39:27 +0800

    2024 day 21: use bottom up DP

--- src/aoc2024/day21.rs
+++ src/aoc2024/day21.rs
@@ -1,7 +1,5 @@
 use std::collections::VecDeque;
 
-use ahash::{HashMap, HashMapExt};
-
 fn parse(input: &str) -> Vec<&[u8]> {
     input.trim().lines().map(|line| line.as_bytes()).collect()
 }
@@ -9,34 +7,9 @@ fn parse(input: &str) -> Vec<&[u8]> {
 const NUMERICAL: &[u8] = b"789456123 0A";
 const DIRECTIONAL: &[u8] = b" ^A<v>";
 
-fn shortest_paths(keypad: &[u8], from: u8, to: u8) -> Vec<Vec<u8>> {
-    let m = keypad.len() / 3;
-    let start = keypad.iter().position(|&b| b == from).unwrap() as i8;
-    let end = keypad.iter().position(|&b| b == to).unwrap() as i8;
-
-    let mut dist = vec![i8::MAX; keypad.len()];
-    let mut q = VecDeque::with_capacity(keypad.len());
-    q.push_back((0, start));
-    dist[start as usize] = 0;
-    while let Some((d, k)) = q.pop_front() {
-        if keypad[k as usize] == to || d > dist[k as usize] {
-            continue;
-        }
-        let (i, j) = (k / 3, k % 3);
-        for (i1, j1) in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)] {
-            let k1 = i1 * 3 + j1;
-            if i1 < 0 || j1 < 0 || i1 == m as i8 || j1 == 3 || keypad[k1 as usize] == b' ' {
-                continue;
-            }
-            if d + 1 < dist[k1 as usize] {
-                dist[k1 as usize] = d + 1;
-                q.push_back((d + 1, k1));
-            }
-        }
-    }
-
-    let mut result = Vec::with_capacity(2);
-    let mut q = Vec::with_capacity(keypad.len());
+fn get_path(rows: i8, dist: &[i8], start: i8, end: i8) -> Vec<Vec<u8>> {
+    let mut result = Vec::with_capacity(9);
+    let mut q = Vec::with_capacity(6);
     q.push((end, vec![b'A']));
     while let Some((k, path)) = q.pop() {
         if k == start {
@@ -53,7 +26,7 @@ fn shortest_paths(keypad: &[u8], from: u8, to: u8) -> Vec<Vec<u8>> {
             let k1 = i1 * 3 + j1;
             if i1 < 0
                 || j1 < 0
-                || i1 == m as i8
+                || i1 == rows
                 || j1 == 3
                 || dist[k1 as usize] != dist[k as usize] - 1
             {
@@ -68,60 +41,170 @@ fn shortest_paths(keypad: &[u8], from: u8, to: u8) -> Vec<Vec<u8>> {
     result
 }
 
-fn directional(code: Vec<u8>, depth: u8, cache: &mut HashMap<(Vec<u8>, u8), usize>) -> usize {
-    let k = (code, depth);
-    if let Some(&r) = cache.get(&k) {
-        return r;
-    }
-    let (mut result, mut curr) = (0, b'A');
-    for &b in &k.0 {
-        let paths = shortest_paths(DIRECTIONAL, curr, b);
-        result += if depth == 1 {
-            paths.iter().map(|p| p.len()).min().unwrap()
-        } else {
-            paths
-                .into_iter()
-                .map(|p| directional(p, depth - 1, cache))
-                .min()
-                .unwrap()
-        };
-        curr = b;
-    }
-    cache.insert(k, result);
-    result
+struct NumericalKeypad {
+    paths: Vec<Vec<Vec<u8>>>,
 }
 
-fn numerical(
-    from: u8,
-    to: u8,
-    depth: u8,
-    cache_num: &mut HashMap<(u8, u8), usize>,
-    cache_dir: &mut HashMap<(Vec<u8>, u8), usize>,
-) -> usize {
-    if let Some(&r) = cache_num.get(&(from, to)) {
-        return r;
+impl NumericalKeypad {
+    fn new() -> Self {
+        let mut paths = Vec::with_capacity(11 * 11);
+        for i in 0..11 {
+            let key = Self::key_code(i);
+            let start = NUMERICAL.iter().position(|&b| b == key).unwrap() as i8;
+            let dist = shortest_paths(NUMERICAL, start);
+            for j in 0..11 {
+                let key = Self::key_code(j);
+                let end = NUMERICAL.iter().position(|&b| b == key).unwrap() as i8;
+                paths.push(get_path(4, &dist, start, end));
+            }
+        }
+        Self { paths }
     }
-    let result = shortest_paths(NUMERICAL, from, to)
-        .into_iter()
-        .map(|p| directional(p, depth, cache_dir))
-        .min()
-        .unwrap();
 
-    cache_num.insert((from, to), result);
-    result
+    fn key_code(index: usize) -> u8 {
+        match index {
+            0..=9 => b'0' + index as u8,
+            10 => b'A',
+            _ => unreachable!(),
+        }
+    }
+
+    fn key_index(code: u8) -> usize {
+        match code {
+            b'0'..=b'9' => (code - b'0') as usize,
+            b'A' => 10,
+            _ => unreachable!(),
+        }
+    }
 }
 
-fn solve(codes: &[&[u8]], depth: u8) -> usize {
-    let mut cache_num = HashMap::with_capacity(11 * 11);
-    let mut cache_dir = HashMap::with_capacity(1024);
+struct DirectionalKeypad {
+    paths: Vec<Vec<Vec<u8>>>,
+}
+
+impl DirectionalKeypad {
+    fn new() -> Self {
+        let mut paths = Vec::with_capacity(5 * 5);
+        for i in 0..5 {
+            let key = Self::key_code(i);
+            let start = DIRECTIONAL.iter().position(|&b| b == key).unwrap() as i8;
+            let dist = shortest_paths(DIRECTIONAL, start);
+            for j in 0..5 {
+                let key = Self::key_code(j);
+                let end = DIRECTIONAL.iter().position(|&b| b == key).unwrap() as i8;
+                paths.push(get_path(2, &dist, start, end));
+            }
+        }
+        Self { paths }
+    }
+
+    fn key_code(index: usize) -> u8 {
+        match index {
+            0 => b'^',
+            1 => b'<',
+            2 => b'v',
+            3 => b'>',
+            4 => b'A',
+            _ => unreachable!(),
+        }
+    }
+
+    fn key_index(code: u8) -> usize {
+        match code {
+            b'^' => 0,
+            b'<' => 1,
+            b'v' => 2,
+            b'>' => 3,
+            b'A' => 4,
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn shortest_paths(keypad: &[u8], start: i8) -> Vec<i8> {
+    let m = keypad.len() / 3;
+    let mut dist = vec![i8::MAX; keypad.len()];
+    let mut q = VecDeque::with_capacity(keypad.len());
+    q.push_back((0, start));
+    dist[start as usize] = 0;
+    while let Some((d, k)) = q.pop_front() {
+        if d > dist[k as usize] {
+            continue;
+        }
+        let (i, j) = (k / 3, k % 3);
+        for (i1, j1) in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)] {
+            let k1 = i1 * 3 + j1;
+            if i1 < 0 || j1 < 0 || i1 == m as i8 || j1 == 3 || keypad[k1 as usize] == b' ' {
+                continue;
+            }
+            if d + 1 < dist[k1 as usize] {
+                dist[k1 as usize] = d + 1;
+                q.push_back((d + 1, k1));
+            }
+        }
+    }
+    dist
+}
+
+fn solve(codes: &[&[u8]], directional_robots: u8) -> usize {
+    let dir_keypad = DirectionalKeypad::new();
+    let mut f_d = [[0; 5]; 5];
+    for d in 0..directional_robots {
+        let mut next = [[0; 5]; 5];
+        for (i, r) in next.iter_mut().enumerate() {
+            for (j, f) in r.iter_mut().enumerate() {
+                let paths = &dir_keypad.paths[i * 5 + j];
+                if d == 0 {
+                    *f = paths.iter().map(|p| p.len()).min().unwrap();
+                } else {
+                    let mut min = usize::MAX;
+                    for p in paths {
+                        let len = p
+                            .iter()
+                            .fold((0, DirectionalKeypad::key_index(b'A')), |(len, i), &b| {
+                                let j = DirectionalKeypad::key_index(b);
+                                (len + f_d[i][j], j)
+                            })
+                            .0;
+                        min = min.min(len);
+                    }
+                    *f = min;
+                }
+            }
+        }
+        f_d = next;
+    }
+
+    let num_keypad = NumericalKeypad::new();
+    let mut f_n = [[0; 11]; 11];
+    for (i, r) in f_n.iter_mut().enumerate() {
+        for (j, f) in r.iter_mut().enumerate() {
+            let paths = &num_keypad.paths[i * 11 + j];
+            let mut min = usize::MAX;
+            for p in paths {
+                let len = p
+                    .iter()
+                    .fold((0, DirectionalKeypad::key_index(b'A')), |(len, i), &b| {
+                        let j = DirectionalKeypad::key_index(b);
+                        (len + f_d[i][j], j)
+                    })
+                    .0;
+                min = min.min(len);
+            }
+            *f = min;
+        }
+    }
+
     codes
         .iter()
         .map(|code| {
-            let (mut len, mut curr) = (0, b'A');
-            for &b in *code {
-                len += numerical(curr, b, depth, &mut cache_num, &mut cache_dir);
-                curr = b;
-            }
+            let len = code
+                .iter()
+                .fold((0, NumericalKeypad::key_index(b'A')), |(len, i), &b| {
+                    let j = NumericalKeypad::key_index(b);
+                    (len + f_n[i][j], j)
+                })
+                .0;
             let num: usize = std::str::from_utf8(&code[..code.len() - 1])
                 .unwrap()
                 .parse()
@@ -135,7 +218,7 @@ pub fn main() {
     let input: String = std::fs::read_to_string("data/2024/day21").unwrap();
     let codes = parse(&input);
     println!("part1: {}", solve(&codes, 2));
-    println!("part1: {}", solve(&codes, 25));
+    println!("part2: {}", solve(&codes, 25));
 }
 
 #[cfg(test)]
