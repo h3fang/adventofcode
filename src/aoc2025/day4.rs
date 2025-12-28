401976b3eeaa1dccb1f2e26cd138576f388f7950
Fang He <hf.enigma@gmail.com>
2025-12-28 21:46:42 +0800

    2025 day 4: optimization

--- src/aoc2025/day4.rs
+++ src/aoc2025/day4.rs
@@ -1,70 +1,75 @@
 use ahash::{HashSet, HashSetExt};
 
-fn parse(data: &str) -> Vec<&[u8]> {
-    data.trim().lines().map(|s| s.as_bytes()).collect()
+fn parse(data: &str) -> Vec<Vec<u8>> {
+    data.trim().lines().map(|s| s.as_bytes().to_vec()).collect()
 }
 
-fn part1(grid: &[&[u8]]) -> usize {
-    let mut ans = 0;
+fn is_accessible(grid: &[Vec<u8>], i: i32, j: i32) -> bool {
     let (m, n) = (grid.len(), grid[0].len());
+    let mut adj = 0;
+    for i1 in [i - 1, i, i + 1] {
+        for j1 in [j - 1, j, j + 1] {
+            if i1 < 0 || i1 == m as i32 || j1 < 0 || j1 == n as i32 || (i1, j1) == (i, j) {
+                continue;
+            }
+            adj += i32::from(grid[i1 as usize][j1 as usize] == b'@');
+            if adj == 4 {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn accessible_places(grid: &[Vec<u8>]) -> Vec<i32> {
+    let (m, n) = (grid.len(), grid[0].len());
+    let mut ans = Vec::with_capacity(m * n);
     for (i, row) in grid.iter().enumerate() {
         for (j, &cell) in row.iter().enumerate() {
             if cell != b'@' {
                 continue;
             }
             let (i, j) = (i as i32, j as i32);
-            let mut adjacent_papers = 0;
-            for i1 in [i - 1, i, i + 1] {
-                for j1 in [j - 1, j, j + 1] {
-                    if i1 < 0 || i1 == m as i32 || j1 < 0 || j1 == n as i32 || (i1, j1) == (i, j) {
-                        continue;
-                    }
-                    adjacent_papers += i32::from(grid[i1 as usize][j1 as usize] == b'@');
-                }
+            if is_accessible(grid, i, j) {
+                ans.push(i * n as i32 + j);
             }
-            ans += usize::from(adjacent_papers < 4);
         }
     }
     ans
 }
 
-fn part2(grid: &[&[u8]]) -> usize {
+fn part2(mut grid: Vec<Vec<u8>>, mut to_remove: Vec<i32>) -> usize {
     let (m, n) = (grid.len(), grid[0].len());
-    let mut grid = grid.iter().map(|r| r.to_vec()).collect::<Vec<_>>();
     let mut ans = 0;
-    let mut papers = HashSet::with_capacity(m * n);
-    for (i, row) in grid.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            if cell == b'@' {
-                papers.insert((i * n + j) as u16);
-            }
-        }
-    }
-    let mut removed = Vec::with_capacity(papers.len());
-    loop {
-        for &idx in &papers {
-            let (i, j) = ((idx as usize / n) as i16, ((idx as usize) % n) as i16);
-            let mut adjacent_papers = 0;
-            for i1 in [i - 1, i, i + 1] {
-                for j1 in [j - 1, j, j + 1] {
-                    if i1 < 0 || i1 == m as i16 || j1 < 0 || j1 == n as i16 || (i1, j1) == (i, j) {
-                        continue;
-                    }
-                    adjacent_papers += i32::from(grid[i1 as usize][j1 as usize] == b'@');
-                }
-            }
-            if adjacent_papers < 4 {
-                removed.push((i, j));
-            }
-        }
-        if removed.is_empty() {
-            break;
-        }
-        ans += removed.len();
-        for (i, j) in removed.drain(..) {
-            papers.remove(&((i * n as i16 + j) as u16));
+    let mut next = HashSet::with_capacity(m * n);
+    while !to_remove.is_empty() {
+        ans += to_remove.len();
+        for &idx in &to_remove {
+            let (i, j) = (idx / n as i32, idx % n as i32);
             grid[i as usize][j as usize] = b'.';
         }
+
+        for idx in to_remove.drain(..) {
+            let (i, j) = (idx / n as i32, idx % n as i32);
+            for i1 in [i - 1, i, i + 1] {
+                for j1 in [j - 1, j, j + 1] {
+                    if i1 < 0
+                        || i1 == m as i32
+                        || j1 < 0
+                        || j1 == n as i32
+                        || (i1, j1) == (i, j)
+                        || grid[i1 as usize][j1 as usize] != b'@'
+                    {
+                        continue;
+                    }
+                    if is_accessible(&grid, i1, j1) {
+                        next.insert(i1 * n as i32 + j1);
+                    }
+                }
+            }
+        }
+
+        to_remove.extend(next.drain());
     }
     ans
 }
@@ -72,8 +77,9 @@ fn part2(grid: &[&[u8]]) -> usize {
 pub fn main() {
     let data = std::fs::read_to_string("data/2025/day4").unwrap();
     let grid = parse(&data);
-    println!("part1: {}", part1(&grid));
-    println!("part2: {}", part2(&grid));
+    let accessible = accessible_places(&grid);
+    println!("part1: {}", accessible.len());
+    println!("part2: {}", part2(grid, accessible));
 }
 
 #[cfg(test)]
@@ -94,7 +100,8 @@ mod tests {
 .@@@@@@@@.
 @.@.@@@.@.";
         let grid = parse(data);
-        assert_eq!(13, part1(&grid));
-        assert_eq!(43, part2(&grid));
+        let accessible = accessible_places(&grid);
+        assert_eq!(13, accessible.len());
+        assert_eq!(43, part2(grid, accessible));
     }
 }
